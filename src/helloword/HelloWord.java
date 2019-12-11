298f544acbac081eccde4a99ca5fc5e397b31397
25040940 <58763260+25040940@users.noreply.github.com>
2019-12-11 15:29:55 +0800

    太差了
    希望你多写代码

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -2,6 +2,7 @@ package helloword;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("HelloWord!!!  Ëæ±ã¸ÄÒ»¸Ä");
+				System.out.println("HelloWord!!!  dfdfdf");	
+        int i=0;				
 	}
 }
